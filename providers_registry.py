8455c1b57dc9e07c11a337138ee4c81a96c08368
gooriwww-1004 <gooriwww@gmail.com>
2026-03-29 06:54:40 +0900

    Add files via upload

--- providers_registry.py
+++ providers_registry.py
@@ -1,11 +1,12 @@
-from typing import Awaitable, Callable, Dict
+from typing import Callable, Dict
 from providers import call_gemini_A, call_gemini_B, call_gemini_C, call_gemini_D
 
-ProviderFunc = Callable[[str], Awaitable[str]]
+# 동기 함수 타입
+ProviderFunc = Callable[[str], str]
 
 PROVIDERS: Dict[str, ProviderFunc] = {
-    "gemini_A": call_gemini_A,  # 분석가
-    "gemini_B": call_gemini_B,  # 검증가
-    "gemini_C": call_gemini_C,  # 선생님
-    "gemini_D": call_gemini_D,  # 브레인스토머
+    "gemini_A": call_gemini_A,
+    "gemini_B": call_gemini_B,
+    "gemini_C": call_gemini_C,
+    "gemini_D": call_gemini_D,
 }
